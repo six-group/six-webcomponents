e0361fb4d58034e8b5ca46a9f56f710f5b709586
Fines, Brice <brice.fines@six-group.com>
2023-02-21 14:06:08 +0100

    COMSLI-600: Demo App: Datepicker in users form is still using string and thus not working

--- source/examples/angular/six-app/src/app/users/components/users/custom-validation.ts
+++ source/examples/angular/six-app/src/app/users/components/users/custom-validation.ts
@@ -4,7 +4,7 @@ import { customMessage } from '../../utils/custom-message';
 export const CustomValidators = {
   requiredTrue: customMessage(Validators.requiredTrue, 'This field should be checked.'),
   required: customMessage(Validators.required, 'This field is required.'),
-  email: customMessage(Validators.email, 'Email is not vaild.'),
+  email: customMessage(Validators.email, 'Email is not valid.'),
   match: (name: string) => (control: FormControl) => {
     if (control.parent?.value[name] !== control.value) {
       return { 'Password does not match.': true };
