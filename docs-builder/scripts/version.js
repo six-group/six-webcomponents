72684647e47c1bb1d5b591c54bd7ddc772f2858a
ibirrer <ibirrer@gmail.com>
2023-08-09 20:19:10 +0200

    remove old docs

--- docs-builder/scripts/version.js
+++
@@ -1,9 +0,0 @@
-const git = require('git-rev-sync');
-const fse = require('fs-extra');
-const log = require('simple-node-logger').createSimpleLogger();
-const { version } = require('../../source/packages/ui-library/package');
-
-const short = git.short();
-
-log.info(`Building docs for version: "${version}" short: "${short}"`);
-fse.writeJsonSync('src/.vuepress/lib/version-details.json', { version, short });
