ecc6f4f5637077830c8d41e257393bc728ec2aa1
ibirrer <ibirrer@gmail.com>
2023-08-14 14:49:43 +0000

    Deploying to gh-pages from @ six-group/six-webcomponents@2ddea3b03653d2bc9ea6e2880baebd64955afdd8 🚀

---
+++ docs/v4/assets/chunks/types-59b455aa.cc5ade9e.js
@@ -0,0 +1,1 @@
+var t;(function(E){E.NUMBER="number",E.LETTER="letter",E.CAPITAL_LETTER="capital-letter",E.LOWER_LETTER="lower-letter",E.CUSTOM="custom"})(t||(t={}));var a;(function(E){E.BEFORE="before",E.AFTER="after"})(a||(a={}));export{t as I,a};
