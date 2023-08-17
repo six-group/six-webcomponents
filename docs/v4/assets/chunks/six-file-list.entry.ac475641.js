1267cbdaabe0922c6ba4e892ca828267da506951
ibirrer <ibirrer@gmail.com>
2023-08-17 19:22:33 +0000

    Deploying to gh-pages from @ six-group/six-webcomponents@2c681a89b8b798befcb7cec2430845740d35f4a0 🚀

---
+++ docs/v4/assets/chunks/six-file-list.entry.ac475641.js
@@ -0,0 +1,1 @@
+import{r as t,h as s}from"../app.1867b66c.js";import"./framework.f9831273.js";import"./theme.f11008ca.js";const e=".six-files-list__container{padding:1rem}.six-files-list__container :last-child{padding-bottom:0}",l=class{constructor(i){t(this,i)}render(){return s("div",{class:"six-files-list__container"},s("slot",null))}};l.style=e;export{l as six_file_list};
