7b6dcdda1354d100d7e1909d9e0873545c0300c5
ibirrer <ibirrer@gmail.com>
2023-08-29 12:13:23 +0000

    Deploying to gh-pages from @ six-group/six-webcomponents@5b240788a35f3e093f597110e9a743f87a2f77b2 🚀

--- docs/assets/chunks/six-error.entry.41c8e57e.js
+++
@@ -1,1 +0,0 @@
-import{r as s,h as r}from"../app.4c672ebb.js";import"./framework.2094cf97.js";import"./theme.24942b4d.js";const t=".six-error-container{text-align:left;color:var(--six-color-danger-800);width:100%}",e=class{constructor(o){s(this,o)}render(){return r("div",{class:"six-error-container"},r("slot",null))}};e.style=t;export{e as six_error};
