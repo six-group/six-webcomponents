698931bf20078880d176caaeb723a21c945b942a
colinscz <19342760+colinscz@users.noreply.github.com>
2023-10-25 07:13:15 +0000

    Deploying to gh-pages from @ six-group/six-webcomponents@f37af2a4c586120c5d311777e19bfee0e443520e 🚀

--- docs/assets/chunks/six-error.entry.31828725.js
+++
@@ -1,1 +0,0 @@
-import{r as s,h as r}from"../app.eaa7d215.js";import"./framework.2094cf97.js";import"./theme.05ad4c6e.js";const t=".six-error-container{text-align:left;color:var(--six-color-danger-800);width:100%}",e=class{constructor(o){s(this,o)}render(){return r("div",{class:"six-error-container"},r("slot",null))}};e.style=t;export{e as six_error};
