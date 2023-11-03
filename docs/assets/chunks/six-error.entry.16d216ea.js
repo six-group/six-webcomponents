0322c9338596ffb0d592319f1736120b15ed6a62
Riatomerehentroweard <20433303+Riatomerehentroweard@users.noreply.github.com>
2023-11-03 12:37:07 +0000

    Deploying to gh-pages from @ six-group/six-webcomponents@ddecb41f978940926c9728f1a7226417fe8bfa6a 🚀

--- docs/assets/chunks/six-error.entry.16d216ea.js
+++
@@ -1,1 +0,0 @@
-import{r as s,h as r}from"../app.7f4b9a8f.js";import"./framework.2094cf97.js";import"./theme.d34f0c99.js";const t=".six-error-container{text-align:left;color:var(--six-color-danger-800);width:100%}",e=class{constructor(o){s(this,o)}render(){return r("div",{class:"six-error-container"},r("slot",null))}};e.style=t;export{e as six_error};
