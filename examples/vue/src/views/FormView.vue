c97d8ef8e5b075f55af6ec75b4ae6665a0d0e793
Birrer, Iwan <iwan.birrer@six-group.com>
2023-11-15 13:19:08 +0100

    feat: very basic vue example

---
+++ examples/vue/src/views/FormView.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+export default {
+  data() {
+    return { text: '', count: 0 };
+  },
+};
+</script>
+
+<template>
+  <section>
+    <six-input v-model="text" label="Text"></six-input>
+    <six-range v-model="count" label="Count" min="0" max="100" step="1"></six-range>
+  </section>
+
+  <div>
+    <p>Text: {{ text }}</p>
+    <p>Count: {{ count }}</p>
+  </div>
+</template>
+
+<style>
+section {
+  display: flex;
+  flex-direction: column;
+  gap: var(--six-spacing-large);
+  width: 300px;
+}
+</style>
