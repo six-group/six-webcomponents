fb7e556a72a83c249beb3f1800659205c23bf73e
Philipp Denzler <philippd@gmail.com>
2023-12-17 15:17:28 +0100

    Add comment about navManager

--- libraries/ui-library-vue/src/lib/plugin.ts
+++ libraries/ui-library-vue/src/lib/plugin.ts
@@ -15,6 +15,8 @@ export const SixWebComponents: Plugin = {
     defineCustomElements();
 
     if (options.router) {
+      // The Stencil generated Vue components expect a navManager instance for router-link support.
+      // See: ./stencil-generated/vue-component-lib/utils.ts for the router-link handling.
       const navManager = {
         navigate: (payload: { event: Event; routerLink: string }) => {
           options.router.push(payload.routerLink);
