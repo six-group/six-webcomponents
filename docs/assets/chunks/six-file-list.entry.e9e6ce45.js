688143232f62f33b73cea7c67ae6a879cd07887a
philippd <philippd@gmail.com>
2024-01-23 13:16:19 +0000

    Deploying to gh-pages from @ six-group/six-webcomponents@804b89a23d91672e7a7d6cc3beebff12788954f2 🚀

--- docs/assets/chunks/six-file-list.entry.e9e6ce45.js
+++
@@ -1,1 +0,0 @@
-import{r as t,h as s}from"../app.e94d8a2f.js";import"./framework.7a2bf33b.js";import"./theme.9f334d6b.js";const e=".six-files-list__container{padding:1rem}.six-files-list__container :last-child{padding-bottom:0}",l=class{constructor(i){t(this,i)}render(){return s("div",{class:"six-files-list__container"},s("slot",null))}};l.style=e;export{l as six_file_list};
