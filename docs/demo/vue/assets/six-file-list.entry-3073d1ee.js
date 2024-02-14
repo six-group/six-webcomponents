be44029c9ccaa4175ff2f432ea530b696f54fdbd
colinscz <19342760+colinscz@users.noreply.github.com>
2024-02-14 14:05:08 +0000

    Deploying to gh-pages from @ six-group/six-webcomponents@f0159053476aa986ff93e9f83bab9587ad859a9a 🚀

--- docs/demo/vue/assets/six-file-list.entry-3073d1ee.js
+++
@@ -1,1 +0,0 @@
-import{r as t,h as s}from"./index-1daffa60.js";const e=".six-files-list__container{padding:1rem}.six-files-list__container :last-child{padding-bottom:0}",l=class{constructor(i){t(this,i)}render(){return s("div",{class:"six-files-list__container"},s("slot",null))}};l.style=e;export{l as six_file_list};
