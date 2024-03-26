d4788e4b245a9eefb2fa4e00963a81531ca6ebc8
belahorvath <47146747+belahorvath@users.noreply.github.com>
2024-03-26 17:41:36 +0000

    Deploying to gh-pages from @ six-group/six-webcomponents@02b9434c8aa0aa0e1d0bc5db616f3811d2a74db9 🚀

--- docs/demo/vue/assets/six-error.entry-d65116dc.js
+++
@@ -1,1 +0,0 @@
-import{r as s,h as r}from"./index-88c5feb6.js";const o=".six-error-container{text-align:left;color:var(--six-color-danger-800);width:100%}",c=o,t=class{constructor(e){s(this,e)}render(){return r("div",{key:"b0a1e9d3d8a88fdcb4d08577ca972785f6deb159",class:"six-error-container"},r("slot",{key:"ea6806c05290c1c9bce2484c7a64ed4cd5d37413"}))}};t.style=c;export{t as six_error};
