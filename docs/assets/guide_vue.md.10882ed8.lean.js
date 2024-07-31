ac7d835ee52a06c272e6bacfb07f58fe5ce5a7fe
colinscz <19342760+colinscz@users.noreply.github.com>
2024-07-31 08:52:57 +0000

    Deploying to gh-pages from @ six-group/six-webcomponents@16f8061577212978aa50e4dafbe891ea752317e6 🚀

--- docs/assets/guide_vue.md.10882ed8.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o as a,c as o,X as e}from"./chunks/framework.57535aa4.js";const d=JSON.parse('{"title":"Vue","description":"","frontmatter":{},"headers":[],"relativePath":"guide/vue.md","filePath":"guide/vue.md"}'),n={name:"guide/vue.md"},l=e("",21),p=[l];function t(r,c,i,y,D,F){return a(),o("div",null,p)}const h=s(n,[["render",t]]);export{d as __pageData,h as default};
