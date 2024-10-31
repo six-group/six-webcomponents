08265f530fb4cd75405863e68ff09bf8747ed9c2
gempi <hello@matthiasgemperli.ch>
2024-10-31 11:57:11 +0000

    Deploying to gh-pages from @ six-group/six-webcomponents@513eb550b89b128bb26e0b1c563305d145d250d0 🚀

---
+++ docs/assets/chunks/channel.B8hHdcMl.js
@@ -0,0 +1,1 @@
+import{U as a,E as n}from"../app.D59AsA3f.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ docs/assets/chunks/clone.BpW4tAIX.js
@@ -0,0 +1,1 @@
+import{b as r}from"./baseUniq.e-9qhSZH.js";var e=4;function a(o){return r(o,e)}export{a as c};
