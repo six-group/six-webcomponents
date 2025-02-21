5bdfdc788c6827564edc5db55ee716d6a7f11ff7
colinscz <19342760+colinscz@users.noreply.github.com>
2025-02-21 14:52:50 +0100

    chore: update lockfile and copy react build into docs, update usage to correct font

--- examples/react/app/layout.tsx
+++ examples/react/app/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import '@six-group/ui-library/dist/ui-library/ui-library.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -25,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
