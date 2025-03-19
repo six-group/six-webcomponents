72e01b60b66fc30de993049b0db895ea772be76e
colinscz <19342760+colinscz@users.noreply.github.com>
2025-03-19 11:11:13 +0100

    fix: addressed issue with routing and menu toggles, corrected some css issues

--- examples/react/app/components/RightSidebar.tsx
+++ examples/react/app/components/RightSidebar.tsx
@@ -1,5 +1,6 @@
 import { SixCard, SixSidebar } from '@six-group/ui-library-react';
 import { Task } from '@/app/types/Task';
+import styles from './right-sidebar.module.css';
 
 interface NavigationProps {
   open: boolean;
@@ -8,9 +9,9 @@ interface NavigationProps {
 
 export default function RightSidebar({ open, tasks }: NavigationProps) {
   return (
-    <SixSidebar slot="right-sidebar" position="right" open={open}>
+    <SixSidebar slot="right-sidebar" position="right" open={open} className={styles.sidebarBg}>
       {tasks.map((task) => (
-        <SixCard>
+        <SixCard key={task.id}>
           <h3>Task #{task.id}</h3>
           <p>{task.title}</p>
         </SixCard>
--- examples/react/app/layout.tsx
+++ examples/react/app/layout.tsx
@@ -5,6 +5,9 @@ import Header from '@/app/components/header';
 import SideNavigation from '@/app/components/side-navigation';
 import { useModalState } from '@/app/hooks/useModalState';
 import RightSidebar from '@/app/components/RightSidebar';
+import styles from './home.module.scss';
+import { Task } from '@six-group/demo-app-angular/src/app/services/tasks.service';
+import { useEffect, useState } from 'react';
 
 const SixRootComponent = SixRoot as React.ComponentType<any>;
 
@@ -13,16 +16,30 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    async function fetchAll() {
+      const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+      const data = await res.json();
+      setTasks(data);
+    }
+    fetchAll();
+  }, []);
+
   const leftSidebar = useModalState(true);
+  const rightSidebar = useModalState(false);
 
   return (
     <html lang="en">
       <body className="">
-        <SixRootComponent stage="DEV" version="DEV-1.2.3" padded={false}>
-          <Header taskCount={1} toggleMenu={leftSidebar.toggle} />
+        <SixRootComponent stage="DEV" version="1.0.0" padded={false} className={styles.homeRoot}>
+          <Header taskCount={1} toggleMenu={leftSidebar.toggle} toggleNotificationMenu={rightSidebar.toggle} />
           <SideNavigation open={leftSidebar.isOpen} />
-          <RightSidebar open={false} tasks={[]} />
-          <main slot="main">{children}</main>
+          <RightSidebar open={rightSidebar.isOpen} tasks={tasks} />
+          <main slot="main" className={styles.mainPadding}>
+            {children}
+          </main>
         </SixRootComponent>
       </body>
     </html>
