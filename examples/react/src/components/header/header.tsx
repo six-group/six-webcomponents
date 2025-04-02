05ca75e7a05fd212da59e493981c42f97944a871
arkadiusz22 <45211274+arkadiusz22@users.noreply.github.com>
2025-04-02 14:27:37 +0200

    add routes, update tab groups panels, remove rightSidebar

--- examples/react/src/components/header/header.tsx
+++ examples/react/src/components/header/header.tsx
@@ -20,20 +20,18 @@ import { useState } from 'react';
 import styles from './header.module.css';
 
 interface HeaderProps {
-  taskCount: number;
-  toggleMenu: () => void;
-  toggleNotificationMenu: () => void;
+  leftSidebar: {
+    isOpen: boolean;
+    toggle: () => void;
+  };
 }
 
-const SixHeaderComponent = SixHeader as React.ComponentType<any>;
-
-export function Header({ toggleMenu, taskCount, toggleNotificationMenu }: HeaderProps) {
+export function Header({ leftSidebar }: HeaderProps) {
   const availableLangs: Language[] = [...languages];
   const apps = ['Application 1', 'Application 2', 'Application 3', 'Application 4'];
   let currentApp = 'Application 1';
 
   const [openSearch, setOpenSearch] = useState(false);
-  const [sideMenuOpen, setSideMenuOpen] = useState(false);
 
   let lang = useLanguage();
   if (lang == null || !availableLangs.includes(lang)) {
@@ -49,66 +47,52 @@ export function Header({ toggleMenu, taskCount, toggleNotificationMenu }: Header
   }
 
   return (
-    <>
-      <SixHeaderComponent slot="header" openSearch={openSearch} style={{ display: 'block' }}>
-        <SixHeaderItem>
-          <SixIconButton name={sideMenuOpen ? 'menu_open' : 'menu'} onClick={toggleMenu} />
-        </SixHeaderItem>
+    <SixHeader slot="header" openSearch={openSearch} style={{ display: 'block' }}>
+      <SixHeaderItem>
+        <SixIconButton name={leftSidebar.isOpen ? 'menu_open' : 'menu'} onClick={leftSidebar.toggle} />
+      </SixHeaderItem>
 
-        <SixHeaderItem>
-          <SixIconButton href={'https://six-group.github.io/six-webcomponents/demo/react/'}>
-            <SixLogo />
-          </SixIconButton>
-        </SixHeaderItem>
+      <SixHeaderItem>
+        <SixIconButton href={'https://six-group.github.io/six-webcomponents/demo/react/'}>
+          <SixLogo />
+        </SixIconButton>
+      </SixHeaderItem>
 
-        {/*search bar */}
-        <SixHeaderItem className="search-icon">
-          <SixIconButton name="search" onClick={() => setOpenSearch(!openSearch)}></SixIconButton>
-        </SixHeaderItem>
+      {/*search bar */}
+      <SixHeaderItem className="search-icon">
+        <SixIconButton name="search" onClick={() => setOpenSearch(!openSearch)}></SixIconButton>
+      </SixHeaderItem>
 
-        <SixSearchField slot="search-field" debounce={600} clearable className={styles.searchIcon} />
+      <SixSearchField slot="search-field" debounce={600} clearable className={styles.searchIcon} />
 
-        {/*notifications */}
-        <SixHeaderItem>
-          <SixIconButton name="notifications_none" onClick={toggleNotificationMenu}>
-            <SixBadge type="danger" pill>
-              {taskCount}
-            </SixBadge>
-          </SixIconButton>
-        </SixHeaderItem>
-
-        {/*App Switcher */}
-        <SixHeaderDropdownItem>
-          <SixHeaderMenuButton slot="trigger">
-            <span>{currentApp}</span>
-            <SixIcon slot="suffix">apps</SixIcon>
-          </SixHeaderMenuButton>
-          <SixMenu>
-            {apps.map((app) => (
-              <SixMenuItem value={app} checked={app === currentApp} onClick={() => (currentApp = app)} key={app}>
-                {app}
-              </SixMenuItem>
-            ))}
-          </SixMenu>
-        </SixHeaderDropdownItem>
-
-        {/*Profile*/}
-        <SixHeaderDropdownItem>
-          <SixIconButton slot="trigger">
-            <SixAvatar image="https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80"></SixAvatar>
-          </SixIconButton>
-          <SixMenu>
-            <SixMenuItem value="change-password">Change password</SixMenuItem>
-            <SixMenuItem value="logout">Logout</SixMenuItem>
-            <SixMenuItem>
-              <SixLanguageSwitcher
-                selected={language}
-                onSixLanguageSwitcherChange={changeLanguage}
-              ></SixLanguageSwitcher>
+      {/*App Switcher */}
+      <SixHeaderDropdownItem>
+        <SixHeaderMenuButton slot="trigger">
+          <span>{currentApp}</span>
+          <SixIcon slot="suffix">apps</SixIcon>
+        </SixHeaderMenuButton>
+        <SixMenu>
+          {apps.map((app) => (
+            <SixMenuItem value={app} checked={app === currentApp} onClick={() => (currentApp = app)} key={app}>
+              {app}
             </SixMenuItem>
-          </SixMenu>
-        </SixHeaderDropdownItem>
-      </SixHeaderComponent>
-    </>
+          ))}
+        </SixMenu>
+      </SixHeaderDropdownItem>
+
+      {/*Profile*/}
+      <SixHeaderDropdownItem>
+        <SixIconButton slot="trigger">
+          <SixAvatar image="https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80"></SixAvatar>
+        </SixIconButton>
+        <SixMenu>
+          <SixMenuItem value="change-password">Change password</SixMenuItem>
+          <SixMenuItem value="logout">Logout</SixMenuItem>
+          <SixMenuItem>
+            <SixLanguageSwitcher selected={language} onSixLanguageSwitcherChange={changeLanguage}></SixLanguageSwitcher>
+          </SixMenuItem>
+        </SixMenu>
+      </SixHeaderDropdownItem>
+    </SixHeader>
   );
 }
