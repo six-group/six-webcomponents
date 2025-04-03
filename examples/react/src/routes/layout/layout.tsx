03384d609ff08b4b17c32660682305840c11009b
arkadiusz22 <45211274+arkadiusz22@users.noreply.github.com>
2025-04-03 13:17:51 +0200

    fix navigation reload and add notifications context and right sidebar

--- examples/react/src/routes/layout/layout.tsx
+++ examples/react/src/routes/layout/layout.tsx
@@ -3,19 +3,25 @@ import { Header } from '@components/header/header';
 import { SideNavigation } from '@components/sideNavigation/sideNavigation';
 import { SixRoot } from '@six-group/ui-library-react';
 import { useModalState } from '@hooks/useModalState';
+import { TasksProvider } from '@components/tasksProvider/tasksProvider';
+import { RightSidebar } from '@components/rightSidebar/rightSidebar';
 
 import styles from './layout.module.css';
 
 export function Layout() {
   const leftSidebar = useModalState(true);
+  const rightSidebar = useModalState(false);
 
   return (
-    <SixRoot padded={true} className={styles['layout-root']}>
-      <Header leftSidebar={leftSidebar} />
-      <SideNavigation open={leftSidebar.isOpen} />
-      <main slot="main" className={styles['layout-padding']}>
-        <Outlet />
-      </main>
-    </SixRoot>
+    <TasksProvider>
+      <SixRoot padded={true} className={styles['layout-root']}>
+        <Header leftSidebar={leftSidebar} toggleRightSidebar={rightSidebar.toggle} />
+        <SideNavigation open={leftSidebar.isOpen} />
+        <RightSidebar isOpen={rightSidebar.isOpen} />
+        <main slot="main" className={styles['layout-padding']}>
+          <Outlet />
+        </main>
+      </SixRoot>
+    </TasksProvider>
   );
 }
