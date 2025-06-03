6c43acd71603a6a88b55a5ce7aefbfaa6c602704
Pennati, Lucas <lucas.pennati@six-group.com>
2025-06-03 15:31:20 +0200

    Added error code on file upload

---
+++ libraries/ui-library/src/components/six-file-upload/types.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @stencil-community/ban-exported-const-enums
+export enum SixFileUploadFailureErrorCode {
+  ONLY_ONE_FILE_ALLOWED = 'ONLY_ONE_FILE_ALLOWED',
+  ONE_OR_MORE_FILES_HAVE_INVALID_MIME_TYPE = 'ONE_OR_MORE_FILES_HAVE_INVALID_MIME_TYPE',
+  INVALID_MIME_TYPE = 'INVALID_MIME_TYPE',
+  ONE_OR_MULTIPLE_FILES_TOO_BIG = 'ONE_OR_MULTIPLE_FILES_TOO_BIG',
+  FILE_TOO_BIG = 'FILE_TOO_BIG',
+}
