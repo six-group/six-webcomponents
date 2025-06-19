360926ca45bb25c08fda515bff3258b55d01bb4b
fortesp <pedro.goncalves@six-group.com>
2025-06-19 08:34:24 +0200

    chore: more prettier formatting docs: readme.md update and others

---
+++ docs/examples/docs-demo-six-breadcrumbs-item-0.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+
+        <p>Using six-breadcrumbs-item as child elements</p>
+        <six-breadcrumbs>
+          <six-breadcrumbs-item name="A"></six-breadcrumbs-item>
+          <six-breadcrumbs-item name="B"></six-breadcrumbs-item>
+          <six-breadcrumbs-item name="C" disabled></six-breadcrumbs-item>
+        </six-breadcrumbs>
+      
+</div>
+</template>
+<style>
+
+</style>
+<script>
+export default {
+  name: 'docs-demo-six-breadcrumbs-item-0',
+  mounted() {  }
+}
+</script>
