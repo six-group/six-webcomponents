55d614a11fe4aa44cbed1c97f57d2c4fee81fbaf
tkst8 <noe.studiger@six-group.com>
2025-09-22 09:30:52 +0200

    fixed tests

--- libraries/ui-library/src/components/six-icon/test/six-icon.spec.tsx
+++ libraries/ui-library/src/components/six-icon/test/six-icon.spec.tsx
@@ -2,10 +2,10 @@ import { newSpecPage } from '@stencil/core/testing';
 import { SixIcon } from '../six-icon';
 
 describe('six-icon', () => {
-  it('renders', async () => {
+  it('renders with default library (material-icons, outlined, inherit size)', async () => {
     const page = await newSpecPage({
       components: [SixIcon],
-      html: `<six-icon></six-icon>`,
+      html: `<six-icon>home</six-icon>`,
     });
     expect(page.root).toEqualHtml(`
       <six-icon size="inherit">
@@ -14,19 +14,20 @@ describe('six-icon', () => {
             <slot></slot>
           </i>
         </mock:shadow-root>
+        home
       </six-icon>
     `);
   });
 
-  it('renders Material Symbols (outlined) by default with inherit size', async () => {
+  it('renders Material Symbols (outlined) when library="material-symbols"', async () => {
     const page = await newSpecPage({
       components: [SixIcon],
-      html: `<six-icon symbols>home</six-icon>`,
+      html: `<six-icon library="material-symbols">home</six-icon>`,
     });
     expect(page.root).toEqualHtml(`
-      <six-icon size="inherit" symbols="">
+      <six-icon size="inherit" library="material-symbols">
         <mock:shadow-root>
-          <i class="icon--inherit material-symbols-outlined" style="--six-icon-fill: 0;">
+          <i class="icon--inherit material-symbols-outlined">
             <slot></slot>
           </i>
         </mock:shadow-root>
@@ -35,15 +36,15 @@ describe('six-icon', () => {
     `);
   });
 
-  it('renders Material Symbols (filled) when "filled" is set', async () => {
+  it('renders Material Symbols (filled) when library="material-symbols" and filled', async () => {
     const page = await newSpecPage({
       components: [SixIcon],
-      html: `<six-icon symbols filled>home</six-icon>`,
+      html: `<six-icon library="material-symbols" filled>home</six-icon>`,
     });
     expect(page.root).toEqualHtml(`
-      <six-icon size="inherit" symbols="" filled="">
+      <six-icon size="inherit" library="material-symbols" filled="">
         <mock:shadow-root>
-          <i class="icon--inherit material-symbols" style="--six-icon-fill: 1;">
+          <i class="icon--inherit material-symbols">
             <slot></slot>
           </i>
         </mock:shadow-root>
