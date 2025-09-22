a34bdc8d2e55e84acec1bcad4b60b56e5db61c14
tkst8 <noe.studiger@six-group.com>
2025-09-22 15:16:28 +0200

    run build

--- docs/examples/docs-demo-six-icon-4.vue
+++ docs/examples/docs-demo-six-icon-4.vue
@@ -1,14 +1,14 @@
 <template>
 <div>
 
-      <div style="display: flex; gap: 1em; min-height: 5rem; align-items: center">
-        <six-icon size="xSmall">search</six-icon>
-        <six-icon size="xSmall">done</six-icon>
-        <six-icon size="xSmall">settings</six-icon>
-        <six-icon size="xSmall">home</six-icon>
-        <six-icon size="xSmall">info</six-icon>
-      </div>
-    
+        <div style="display: flex; gap: 1em; min-height: 5rem; align-items: center">
+          <six-icon size="xSmall">search</six-icon>
+          <six-icon size="xSmall">done</six-icon>
+          <six-icon size="xSmall">settings</six-icon>
+          <six-icon size="xSmall">home</six-icon>
+          <six-icon size="xSmall">info</six-icon>
+        </div>
+      
 </div>
 </template>
 <style>
