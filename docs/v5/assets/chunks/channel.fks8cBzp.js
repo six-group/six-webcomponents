6ed336a49479867938ef17380458fa44a4942095
ibirrer <ibirrer@gmail.com>
2025-10-13 08:09:53 +0000

    Deploying to gh-pages from @ six-group/six-webcomponents@b0fd8e9a50bbe6ad3e0322e02e3940e809cca44f 🚀

---
+++ docs/v5/assets/chunks/channel.fks8cBzp.js
@@ -0,0 +1,1 @@
+import{U as a,C as n}from"../app.DGjhY1Fc.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
--- docs/v5/assets/chunks/clone.CWHKTKOF.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./baseUniq.Dind71jp.js";var e=4;function a(o){return r(o,e)}export{a as c};
