6bde190adb317d17b226a4762cb9344c06759987
colinscz <19342760+colinscz@users.noreply.github.com>
2025-10-23 10:53:51 +0000

    Deploying to gh-pages from @ six-group/six-webcomponents@99e1a071dec447b3ae8570912f916be910734166 🚀

---
+++ docs/v5/assets/chunks/chunk-FHKO5MBM.DFi7YpdY.js
@@ -0,0 +1,1 @@
+import{_ as s}from"../app.B0D1wt_6.js";var t,e=(t=class{constructor(i){this.init=i,this.records=this.init()}reset(){this.records=this.init()}},s(t,"ImperativeState"),t);export{e as I};
