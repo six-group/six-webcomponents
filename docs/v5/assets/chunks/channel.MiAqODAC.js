914713f9f80b017feedeebc028ce5319933c308b
colinscz <19342760+colinscz@users.noreply.github.com>
2025-10-23 10:31:14 +0000

    Deploying to gh-pages from @ six-group/six-webcomponents@2b09e557d3156afe395caf9e3d4bb532e3ad0ef1 🚀

---
+++ docs/v5/assets/chunks/channel.MiAqODAC.js
@@ -0,0 +1,1 @@
+import{U as a,C as n}from"../app.D0hfZ913.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
--- docs/v5/assets/chunks/channel.fks8cBzp.js
+++
@@ -1,1 +0,0 @@
-import{U as a,C as n}from"../app.DGjhY1Fc.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
