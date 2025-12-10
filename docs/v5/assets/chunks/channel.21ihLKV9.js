6f8aa5ca0f9cc322f6e3b45fe0d73bcf7d6a5dbf
pennal <pennal@users.noreply.github.com>
2025-12-10 16:21:44 +0000

    Deploying to gh-pages from @ six-group/six-webcomponents@d4f3849771f1fef9e377d6cf5f578fbbf94ca8c1 🚀

---
+++ docs/v5/assets/chunks/channel.21ihLKV9.js
@@ -0,0 +1,1 @@
+import{U as a,C as n}from"../app.mt5KY8sP.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
--- docs/v5/assets/chunks/clone.C2KqZuLj.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./baseUniq.Dl7o38Cg.js";var e=4;function a(o){return r(o,e)}export{a as c};
