9c595fe5c68c4e83068b525b670bd437ffb5609f
rspy <roman.spycher@gmail.com>
2026-03-05 15:05:03 +0000

    Deploying to gh-pages from @ six-group/six-webcomponents@c96d271bcb8f7a5dfd7713439bdeac4cd624f98a 🚀

---
+++ docs/v5/assets/chunks/channel.MfSB5Vjv.js
@@ -0,0 +1,1 @@
+import{U as a,C as n}from"../app.l6Nc8H1z.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
--- docs/v5/assets/chunks/clone.B7oEI98B.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./graph.BRR5pqKk.js";var e=4;function a(o){return r(o,e)}export{a as c};
