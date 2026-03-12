b32f814a5b645d8496b54ebd7805d2e4deacc529
pennal <pennal@users.noreply.github.com>
2026-03-12 15:03:51 +0000

    Deploying to gh-pages from @ six-group/six-webcomponents@1652632d03d9360f42906217a47a3381e8db43d4 🚀

---
+++ docs/v5/assets/chunks/channel.DvCjUdtC.js
@@ -0,0 +1,1 @@
+import{U as a,D as n}from"../app.1ErX-bZ3.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
--- docs/v5/assets/chunks/clone.9lEkvvXO.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./baseUniq.K7YSB3fr.js";var e=4;function a(o){return r(o,e)}export{a as c};
