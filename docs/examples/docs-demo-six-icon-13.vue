3b2a09109f673863232f4dc517c8abe9502bcccb
fortesp <pedro.goncalves@six-group.com>
2026-06-01 10:55:13 +0200

    feat(six-icon): support custom SVGs

---
+++ docs/examples/docs-demo-six-icon-13.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+
+        <div style="display: flex; gap: 1em; min-height: 6rem; align-items: center; fill: red">
+          <six-icon size="xSmall" src="/assets/test.svg" inline-svg></six-icon>
+          <six-icon size="small" src="/assets/test.svg" inline-svg></six-icon>
+          <six-icon size="large" src="/assets/test.svg" inline-svg></six-icon>
+          <six-icon size="xxLarge" src="/assets/test.svg" inline-svg></six-icon>
+        </div>
+      
+</div>
+</template>
+<style>
+
+</style>
+<script>
+export default {
+  name: 'docs-demo-six-icon-13',
+  mounted() {  }
+}
+</script>
