6c2770f390929ac15384b7fcf8dd9cbd6be5fe0e
fortesp <pdfgo@pm.me>
2026-06-17 14:09:30 +0000

    Deploying to gh-pages from @ six-group/six-webcomponents@5d0584895b3220a40bc6c8109ba4752f6442b216 🚀

---
+++ docs/v5/assets/chunks/channel.Ch2JkTjW.js
@@ -0,0 +1,1 @@
+import{U as a,D as n}from"../app.B0rGsTCs.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ docs/v5/assets/chunks/clone.BCLcnfBr.js
@@ -0,0 +1,1 @@
+import{b as r}from"./baseUniq.DIl54_cv.js";var e=4;function a(o){return r(o,e)}export{a as c};
