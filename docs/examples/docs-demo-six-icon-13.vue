c9004b4d9c90d89c77710cc523c0156f3bf4dc49
Colin S. <19342760+colinscz@users.noreply.github.com>
2026-07-03 07:53:45 +0200

    Release/5.5.0 (#544)
    * docs: prepare changelog for release v5.5.0
    
    * docs: fix examples with custom icon
    
    * docs: extend component generation script to handle custom assets with Vitepress helper
    
    * v5.5.0 (#545)
    
    Co-authored-by: GitHub action <>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- docs/examples/docs-demo-six-icon-13.vue
+++ docs/examples/docs-demo-six-icon-13.vue
@@ -2,10 +2,10 @@
 <div>
 
         <div style="display: flex; gap: 1em; min-height: 6rem; align-items: center; fill: red">
-          <six-icon size="xSmall" src="/assets/demo-icon.svg" inline-svg></six-icon>
-          <six-icon size="small" src="/assets/demo-icon.svg" inline-svg></six-icon>
-          <six-icon size="large" src="/assets/demo-icon.svg" inline-svg></six-icon>
-          <six-icon size="xxLarge" src="/assets/demo-icon.svg" inline-svg></six-icon>
+          <six-icon size="xSmall" :src="withBase('/assets/demo-icon.svg')" inline-svg></six-icon>
+          <six-icon size="small" :src="withBase('/assets/demo-icon.svg')" inline-svg></six-icon>
+          <six-icon size="large" :src="withBase('/assets/demo-icon.svg')" inline-svg></six-icon>
+          <six-icon size="xxLarge" :src="withBase('/assets/demo-icon.svg')" inline-svg></six-icon>
         </div>
       
 </div>
@@ -14,8 +14,11 @@
 
 </style>
 <script>
+import { withBase } from 'vitepress';
+
 export default {
   name: 'docs-demo-six-icon-13',
+  methods: { withBase },
   mounted() {  }
 }
 </script>
