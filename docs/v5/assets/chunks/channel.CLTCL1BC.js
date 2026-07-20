0665b8b8f0aa47e0e8c5829bed5e43f1454742b5
colinscz <19342760+colinscz@users.noreply.github.com>
2026-07-20 12:02:29 +0000

    Deploying to gh-pages from @ six-group/six-webcomponents@a321dfc2f3cc6ecd23ef57cfaa49aaa05ab1ded4 🚀

--- docs/v5/assets/chunks/channel.CLTCL1BC.js
+++
@@ -1,1 +0,0 @@
-import{U as a,C as n}from"../app.Cq7YCJ7O.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ docs/v5/assets/chunks/swimlanesDiagram-G3AALYLV.BX_18cYD.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.DW85u9Fu.js";import{_ as a}from"../app.DGzTN4iZ.js";import"./chunk-5VM5RSS4.CuqTLEZb.js";import"./chunk-XXDRQBXY.RwIMrlSt.js";import"./chunk-VR4S4FIN.Dqy8-hUu.js";import"./chunk-32BRIVSS.Bh1eB5zZ.js";import"./channel.BqLd7rhR.js";import"./framework.DYQf_ReZ.js";import"./theme.DJj2cD1N.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
