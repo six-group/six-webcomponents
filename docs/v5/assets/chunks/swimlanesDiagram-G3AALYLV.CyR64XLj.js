b19882eac223f3c35cb4c1fc1e611cf43514ed8e
rspy <roman.spycher@gmail.com>
2026-07-20 07:08:05 +0000

    Deploying to gh-pages from @ six-group/six-webcomponents@737b374e23eedc8464bcd4c8a52269503ba0366a 🚀

--- docs/v5/assets/chunks/swimlanesDiagram-G3AALYLV.CyR64XLj.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U.pInpHjwp.js";import{_ as a}from"../app.DBNa3jXu.js";import"./chunk-5VM5RSS4.78hAgUZo.js";import"./chunk-XXDRQBXY.B3oj8l3P.js";import"./chunk-VR4S4FIN.CgAmmJ0d.js";import"./chunk-32BRIVSS.DyLGbSFG.js";import"./channel.CQ1MjRpL.js";import"./framework.DYQf_ReZ.js";import"./theme.AgOT0WFd.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
