af50e68ebd917813b17aee203addb4afebc75173
colinscz <19342760+colinscz@users.noreply.github.com>
2026-07-24 14:22:06 +0000

    Deploying to gh-pages from @ six-group/six-webcomponents@d6bfa3329cfa5f44571ed6da2615215dc57abbc6 🚀

--- docs/v5/assets/chunks/chunk-2Q5K7J3B.DsjcGLx8.js
+++
@@ -1,1 +0,0 @@
-import{_ as s}from"../app.Cr1RVl_L.js";var t,e=(t=class{constructor(i){this.init=i,this.records=this.init()}reset(){this.records=this.init()}},s(t,"ImperativeState"),t);export{e as I};
---
+++ docs/v5/assets/chunks/swimlanesDiagram-G3AALYLV.Dnuf_ufo.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.D2AkpH5B.js";import{_ as a}from"../app.DSauLetF.js";import"./chunk-5VM5RSS4.9YyQZ7PQ.js";import"./chunk-XXDRQBXY.BZloqSJo.js";import"./chunk-VR4S4FIN.BvZrJPUL.js";import"./chunk-32BRIVSS.BBaqmzT_.js";import"./channel.tJ1CKioA.js";import"./framework.DYQf_ReZ.js";import"./theme.DoolHrtH.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
