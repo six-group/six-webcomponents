26a45c68281ce8093b4acc8920007aa73cf9cea5
colinscz <19342760+colinscz@users.noreply.github.com>
2026-07-27 13:20:44 +0000

    Deploying to gh-pages from @ six-group/six-webcomponents@00a90c078e453afcf237e0685fe7da078d59a3ac 🚀

---
+++ docs/v5/assets/chunks/swimlanesDiagram-G3AALYLV.DyihOhIt.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.CJ2E1p6Q.js";import{_ as a}from"../app.BdHpRYvq.js";import"./chunk-5VM5RSS4.Cb8sP1Sm.js";import"./chunk-XXDRQBXY.CWpqHMjS.js";import"./chunk-VR4S4FIN.D2zojIEH.js";import"./chunk-32BRIVSS.DTl_7ATj.js";import"./channel.BiIATAWf.js";import"./framework.DYQf_ReZ.js";import"./theme.BEGxybLk.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
